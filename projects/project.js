c844227520e2db739bbd0193ddb5645618a5def4
db648 <95957492+db648@users.noreply.github.com>
2022-04-14 01:13:36 +0530

    some changes

--- projects/project.js
+++ projects/project.js
@@ -1,5 +1,26 @@
 const Projects = [
     {
+        name : "Clone of Agoda.com",
+        about : "Agoda is a fastest growing online travel booking platform. It also provides budget hotels, apartments, homes, and villas to suit all budgets and travel occasions.",
+        img : "https://miro.medium.com/max/1400/1*V3SfEMO4g2oOyi9NnEXjDQ.png",
+        gitrepo : "https://github.com/db648/Agoda-clone",
+        blog : "https://medium.com/@vdbhavani.648/agoda-clone-eb28c930815e",
+        tech_stack : "HTML | CSS | JAVASCRIPT | React | Redux | MongoDB | Express | Nodejs",
+        tech_stackicons : [
+            `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+            `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+            `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+            `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+            `<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+            `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+            `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+            `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+            `<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`
+        ],
+        project_link : "https://agoda-clone-website.vercel.app/",
+        project_type : "Team Project"
+    },
+    {
         name : "Clone of specsmakers.in",
         about : "Specsmakers offers a wide variety of stylish eyewear with high quality of Eyeglasses, Eyeglass Frames, Computer Glasses, Contact Lenses and Sunglasses Online at specsmakers.",
         img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT83waNZy94ppQK-oRhV70lco-r4kcA8rZ13Q&usqp=CAU",
@@ -75,6 +96,11 @@ Projects.forEach(pro => {
       ${pro.tech_stackicons[2] ? pro.tech_stackicons[2] : ''}
       ${pro.tech_stackicons[3] ? pro.tech_stackicons[3] : ''}
       ${pro.tech_stackicons[4] ? pro.tech_stackicons[4] : ''}
+      ${pro.tech_stackicons[5] ? pro.tech_stackicons[5] : ''}
+      ${pro.tech_stackicons[6] ? pro.tech_stackicons[6] : ''}
+      ${pro.tech_stackicons[7] ? pro.tech_stackicons[7] : ''}
+      ${pro.tech_stackicons[8] ? pro.tech_stackicons[8] : ''}
+      ${pro.tech_stackicons[9] ? pro.tech_stackicons[9] : ''}
       </span>
       
     </div>
